34387a15692cdf2361dc45f0747b874c3f34cc33
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-07-18 09:58:15 +0300

    fix: add exception handler

--- stats-service/stats-server/src/main/java/ru/practicum/server/ServerService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/server/ServerService.java
@@ -1,16 +1,22 @@
 package ru.practicum.server;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.dto.EndpointRequest;
 import ru.practicum.dto.ViewStats;
 import ru.practicum.server.dto.EndpointHit;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ServerService {
 
@@ -40,5 +46,12 @@ public class ServerService {
                 return serverRepository.findAllViewedEndpointsWithUris(start, end, uris);
             }
         }
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotFoundException(final IllegalArgumentException e) {
+        log.error(e.getMessage());
+        return Map.of(e.getMessage(), HttpStatus.BAD_REQUEST.name());
     }
 }
