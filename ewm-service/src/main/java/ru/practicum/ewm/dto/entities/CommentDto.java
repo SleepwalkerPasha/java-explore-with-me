7ecc8ccca3b2d1eabed98ae1b5c794bb0327fc31
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-07-21 12:43:28 +0300

    fix: unused import and readme

--- ewm-service/src/main/java/ru/practicum/ewm/dto/entities/CommentDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/dto/entities/CommentDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
